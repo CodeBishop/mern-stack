83e75bfaebea45800ecb3f3a370a0d3b71849542
sbishop <sbishop@navarik.com>
2019-01-14 15:47:17 -0800

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://bish:bish22@ds257314.mlab.com:57314/devconnector2'
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+app.get('/', (req, res) => res.send('Hello'))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
