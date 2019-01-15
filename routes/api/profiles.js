57711a92cf47af4670ce9036ab772ddf39bf4927
sbishop <sbishop@navarik.com>
2019-01-15 08:14:01 -0800

    Add directory structure and blank files for API routing.

---
+++ routes/api/profiles.js
@@ -0,0 +1,1 @@
+// A file for stuff related to the in-depth profiles of users (location, bio, social network links, etc).
---
+++ routes/api/users.js
@@ -0,0 +1,1 @@
+// A file for dealing with authentication (username, email, password, etc.)
