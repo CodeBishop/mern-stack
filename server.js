5b348b9587cfd537e34a7806e88d10daeacb7555
sbishop <sbishop@navarik.com>
2019-01-15 08:09:58 -0800

    Add MongoDB connection.

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express()
 // DB Config
 const db = require('./config/keys').mongoURI
 
+// Connect to MongoDB
+mongoose
+  .connect(db, {useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('err'))
+
 app.get('/', (req, res) => res.send('Hello'))
 
 const port = process.env.PORT || 5000
