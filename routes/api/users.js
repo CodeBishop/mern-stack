a0af2b1994c2f8d3a23488140c942f1ca91441b9
sbishop <sbishop@navarik.com>
2019-01-17 10:22:36 -0800

    Add comment.

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,6 +35,7 @@ router.post('/register', (req, res) => {
           password: req.body.password
         })
 
+        // Salt and hash the new user's password.
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if(err) throw err
