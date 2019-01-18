0d345c3a3d96f4149417c9de3e8498e851d77cde
sbishop <sbishop@navarik.com>
2019-01-18 12:26:22 -0800

    Add a Post model for user comment posting.

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema.
+// Note that name and avatar are duplicated in Posts rather than linked. This ensures that comments posted by users will remain intact even if they delete their account.
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Post = mongoose.model('post', PostSchema)
