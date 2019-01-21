7949d3a4de25023ea8cecb01bfc5264fe384d7f8
sbishop <sbishop@navarik.com>
2019-01-21 09:13:20 -0800

    Implement simple HTML footer component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>My React App</h1>
+        <Footer />
       </div>
     );
   }
