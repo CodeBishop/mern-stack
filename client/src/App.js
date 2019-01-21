d3df8b2527b741e2566d7bfa6113c2de04af6fbb
sbishop <sbishop@navarik.com>
2019-01-21 09:39:08 -0800

    Add routing and login/registration.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Navbar from './components/layout/Navbar'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Router>
+    )
   }
 }
 
