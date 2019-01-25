59e4ae063c07e3e5741631ad6ad125320b290aaf
sbishop <sbishop@navarik.com>
2019-01-25 13:40:06 -0800

    Make app logout user with expired access token.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import jwt_decode from 'jwt-decode'
 import { Provider } from 'react-redux'
 import React, { Component } from 'react'
 
-import { setCurrentUser } from './actions/authActions'
+import { logoutUser, setCurrentUser } from './actions/authActions'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
@@ -22,6 +22,17 @@ if(localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded))
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000
+  if(decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser())
+    // TODO: Clear current profile
+
+    // Redirect to login
+    window.location.href = '/login'
+  }
 }
 
 class App extends Component {
