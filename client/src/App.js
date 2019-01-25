cd35bbc6bc37205aa6c25e8a1549da74179d7d36
sbishop <sbishop@navarik.com>
2019-01-25 13:06:14 -0800

    Connect redux to login and make login state persist.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 import { Provider } from 'react-redux'
 import React, { Component } from 'react'
 
+import { setCurrentUser } from './actions/authActions'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Navbar from './components/layout/Navbar'
 import Register from './components/auth/Register'
+import setAuthToken from './utils/setAuthToken'
 import store from  './store'
 
 import './App.css'
+
+// Check for token
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken)
+  // Decode token and get user info and expiration.
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 class App extends Component {
   render() {
